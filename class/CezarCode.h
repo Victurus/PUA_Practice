c1b0dbe982b88f2d4957649945f872ada05fca4f
Victor Davidov <victor56152@gmail.com>
2017-03-09 17:53:58 +0300

    Переписан класс CezarCode. Теперь всё работает.

--- class/CezarCode.h
+++ class/CezarCode.h
@@ -2,201 +2,193 @@
  * Библиотека позволяющая кодировать текст входного файла Кодом Цезаря.
  * Написал: Виктор Давыдов
  * Почта: victor56152@gmail.com
- * Зачем писал: появилсь резкая необходимость(в универе задали)
+ * Зачем писал: в универе задали
  */
 
 #include"Headers.h"
 
 struct ch_coder
 {
-  wchar_t ch;  // символ
-  wint_t from; // исходный код символа - A
-  wint_t to;   // код закодированного символа - B
+  int from;
+  int to;
 };
 
 class CezarCode
 {
   private:
-    string file_tooperate;                 // обрабатываемый файл
-    bool menu;                             // true  - кодирование 
-                                           // false - декодирование
-    wint_t n;                              // НОД(n, m) == 1
-    wint_t k;                              // ключ
-    vector<ch_coder> letters;              // символы 
-    void fill_letters();
+    string operationfile;
+    int k;
+    int n;
+    wstring symbols;
+    bool menu;
+    map<wchar_t, ch_coder> symbolscode;
+    int decodesymbol(int B, int m);
   public:
-    CezarCode(string file_tocode_name, int new_n, int new_k);
-    CezarCode(string file_todecode_name);
+    CezarCode(string filename, int new_k, int new_n);
+    CezarCode(string filename);
 
-    vector<ch_coder> get_lettersCD();
-    void reset_filename(string new_filename);
-    int find_ch(wchar_t ch);
-    int getel_fromdivcolco(int B, int m);
-    void code();
-    void decode();
-    void print_letters();
+    void codefile();
+    void decodefile();
+    void print(string filename);
+
+    ~CezarCode();
 };
 
-CezarCode::CezarCode(string file_tocode_name, int new_n, int new_k)
+CezarCode::CezarCode(string filename, int new_k, int new_n)
 {
-  this->file_tooperate = file_tocode_name;
-  this->n = new_n;
   this->k = new_k;
+  this->n = new_n;
+  this->operationfile = filename;
   this->menu = true;
+  this->symbols = L"";
 }
 
-CezarCode::CezarCode(string file_todecode_name)
+CezarCode::CezarCode(string filename)
 {
-  this->file_tooperate = file_todecode_name;
+  this->operationfile = filename;
   this->menu = false;
+  this->symbols = L"";
 }
 
-vector<ch_coder> CezarCode::get_lettersCD()
+CezarCode::~CezarCode()
+{}
+
+int CezarCode::decodesymbol(int B, int m)
 {
-  return this->letters;
-}
-
-void CezarCode::reset_filename(string new_filename)
-{
-  this->file_tooperate = new_filename;
-}
-
-int CezarCode::find_ch(wchar_t ch)
-{
-  for(int i = 0; i < (int) this->letters.size(); i++)
-  {
-    if (letters[i].ch == ch)
-    {
-      if (menu)
-        return letters[i].to;
-      else
-        return letters[i].from;
-    }
-  }
-  return -1;
-}
-
-void CezarCode::code()
-{
-  if (menu)
-  {
-    // открыл для чтения
-    std::locale::global(std::locale(""));
-    wifstream win(this->file_tooperate.c_str());
-
-    wchar_t c;
-
-    set<wchar_t> reader;
-
-    // читаю уникально, символы и маленькие буквы
-    while(win >> c)
-    {
-      wctrans_t x = wctrans("tolower");
-      wchar_t ch = towctrans(c, x);
-
-      reader.insert(ch);
-    }
-    
-    // заполняю массив кодирования
-    int cnt = 0;
-    int m = (int) reader.size();
-    for(auto it = reader.begin(); it != reader.end(); it++)
-    {
-      ch_coder tmp;
-      tmp.ch = (*it);
-      tmp.from = cnt;
-      tmp.to = (cnt * this->n + this->k) % m;
-      this->letters.push_back(tmp);
-      cnt++;
-    }
-
-    // создаю выходной файл и начинаю заново читать заданный
-    string new_name = this->file_tooperate.substr(0, this->file_tooperate.find('.'));
-    win.close();
-    win.open(this->file_tooperate.c_str());
-    wofstream wout ((new_name + ".out").c_str(), ios::out);
-
-    // записываю данные для декодирования
-    //wprintf(L" %3d %3d %3d\n", this->n, this->k, m);
-    wout << setw(3) << this->n << L" "
-         << setw(3) << this->k << L" "
-         << setw(3) << m       << L"\n";
-    for(int i = 0; i < (int) letters.size(); i++)
-    {
-      //wprintf(L"%lc %3d\n", letters[i].ch, letters[i].to);
-      wout << setw(3) << letters[i].ch << L" "
-           << setw(3) << letters[i].to << L"\n";
-    }
-
-    // вывожу сам текст
-    while(win >> c)
-    {
-      //wprintf(L"%lc", letters[find_ch(c)].ch);
-      wout << letters[find_ch(c)].ch;
-    }
-    
-    // закрываю все потоки
-    win.close();
-    wout.close();
-  }
-  else
-  {
-    wprintf(L"Error: Cur menu - decode");
-  }
-}
-
-int CezarCode::getel_fromdivcolco(int B, int m)
-{
-  int cur = B;
-  while ((cur - k) % n != 0)
+  int cur = B + m;
+  while((cur - this->k) % this->n != 0)
   {
     cur += m;
   }
-  return (cur - k) / n;
+  return abs((cur - this->k) / this->n);
 }
 
-void CezarCode::decode()
+void CezarCode::codefile()
 {
-  if (!menu)
+  if(menu)
   {
-    wifstream win (this->file_tooperate.c_str(), ios::in);
-
-    int m;
-    win >> this->n >> this->k >> m;
-    letters.clear();
-    for(int i = 0; i < m; i++)
-    {
-      ch_coder tmp;
-      win >> tmp.ch >> tmp.to;
-      tmp.from = this->getel_fromdivcolco(tmp.to, m) % m;
-      letters.push_back(tmp);
-    }
-    string new_name = this->file_tooperate.substr(0, this->file_tooperate.find('.'));
-    wofstream wout ((new_name + "_decoded.out").c_str(), ios::out);
+    std::locale::global( std::locale("") );
+    wfstream win(this->operationfile, ios::in);
+    win.unsetf(ios::skipws);
 
     wchar_t c;
+    set<wchar_t> reader;
     while(win >> c)
     {
-      //wprintf(L"%lc", letters[find_ch(c)].ch);
-      wout << letters[find_ch(c)].ch;
+      wctrans_t x = wctrans("tolower");
+      reader.insert( towctrans(c, x) );
+    }
+
+    //Переход в начало исходного файла для повторного чтения
+    win.clear();
+    win.seekg(0, ios_base::beg);
+    string newname = operationfile.substr(0, operationfile.find('.')) + ".coded";
+    wfstream wout(newname, ios::out);
+
+    int cnt = 0;
+    int m = (int) reader.size();
+
+    wout << n << L" "
+         << k << L" "
+         << m << endl;
+
+    for(auto it = reader.begin(); it != reader.end(); it++)
+    {
+      this->symbols += (*it);
+      ch_coder tmp;
+      tmp.from = cnt;
+      tmp.to = (cnt * n + k) % m;
+      switch((*it))
+      {
+        case L' ':
+          wout << L"space" << "\t" << tmp.to << endl;
+          break;
+        case L'\n':
+          wout << L"\\n" << "\t" << tmp.to << endl;
+          break;
+        default:
+          wout << (*it) << "\t" << tmp.to << endl;
+      }
+      this->symbolscode[(*it)] = tmp;
+      cnt++;
+    }
+
+    c = L' ';
+    while(win >> c)
+    {
+      wctrans_t x = wctrans("tolower");
+      wout << symbols[symbolscode[towctrans(c, x)].to];
+    }
+
+    win.close();
+    wout.close();
+  }
+  else
+  {
+    cerr << "Error: Wrong menu choosed" << endl;
+  }
+}
+
+void CezarCode::decodefile()
+{
+  if(!menu)
+  {
+    std::locale::global(std::locale(""));
+    wfstream win(operationfile, ios::in);
+    
+    symbolscode.clear();
+    int m;
+    win >> n >> k >> m;
+    symbols = wstring(m, L' ');
+    for(int i = 0; i < m; i++)
+    {
+      wstring symbtmp;
+      ch_coder tmp;
+      win >> symbtmp >> tmp.to;
+      tmp.from = decodesymbol(tmp.to, m) % m;
+      wchar_t ch;
+      if (symbtmp == L"space")
+          ch = L' ';
+      else if (symbtmp == L"\\n")
+        ch = L'\n';
+      else
+        ch = symbtmp.front();
+      symbols[tmp.to] = ch;
+      symbolscode[ch] = tmp;
+    }
+
+    win.unsetf(ios::skipws);
+    string newname = operationfile.substr(0, operationfile.find('.')) + ".decoded";
+    wfstream wout(newname, ios::out);
+
+    wchar_t c;
+    win >> c;
+    while(win >> c)
+    {
+      int index = symbolscode[c].from;
+      wchar_t symb = symbols[index];
+      wout << symb;
     }
     win.close();
     wout.close();
   }
   else
   {
-    wprintf(L"Error: Cur menu - code");
+    cerr << "Error: Wrong menu choosed" << endl;
   }
 }
 
-void CezarCode::print_letters()
+void CezarCode::print(string filename)
 {
-  wsp(21);
-  wprintf(L" | Smb | sfrom | smbto |\n");
-  wsp(21);
-  for (int i = 0; i < (int) letters.size(); i++)
+  freopen(filename.c_str(), "w", stdout);
+  wcout << k << L"\t"
+        << n << L"\t"
+        << symbolscode.size() << endl;
+  for(auto it = symbolscode.begin(); it != symbolscode.end(); it++)
   {
-    wprintf(L" | %3lc | %5d | %5d |\n", letters[i].ch, letters[i].from, letters[i].to);
+    wcout << (*it).first << L"\t"
+          << (*it).second.from << L"\t" 
+          << (*it).second.to << endl;
   }
-  wsp(21);
+  wcout << symbols;
 }
--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@ int main(int argc, char** argv)
 {
   setlocale(LC_ALL, "");
 
-  CezarCode ccode("read/input.txt", 8, 5);
-  ccode.code();
-  CezarCode cdecode("read/input.out");
-  cdecode.decode();
-  cdecode.print_letters();
+  CezarCode ccode("read/input.txt", 8, 7);
+  ccode.codefile();
+  CezarCode cdecode("read/input.coded");
+  cdecode.decodefile();
 
   return 0;
 }
