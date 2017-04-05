aff5b51b78404c67d4808f8a1262ce1b592dff16
Victor Davidov <victor56152@gmail.com>
2017-04-05 22:58:28 +0300

    Xor кодирование гамма-последовательностью

---
+++ class/GamXorSeq.h
@@ -0,0 +1,196 @@
+/* 
+ * Библиотека позволяющая кодировать текст входного файла XOR на основе
+ * гамма-последовательности(буквенной).
+ * Написал: Виктор Давыдов
+ * Почта: victor56152@gmail.com
+ * Производится работа с расширенными символами wchar_t. Для кодировки
+ * UTF-8 хорошо подходит.
+ */
+
+#include"Headers.h"
+#include<csignal>
+
+class GamXorSeq
+{
+  private:
+    string operationfile;
+    wstring keyword;
+    wstring alphabet;
+    int A;
+    int C;
+    bool menu;
+    void setkeyword();
+  public:
+    GamXorSeq(string operationfile_val, int A_val, int C_val);
+    GamXorSeq(string operationfile_val);
+    ~GamXorSeq();
+
+    void codefile();
+    void decodefile();
+    friend wostream &operator<<(wostream &stream, GamXorSeq o);
+};
+
+void GamXorSeq::setkeyword()
+{
+  wcout << L" Генерация ключа" << endl;
+  srand((unsigned)time(NULL));
+  int T0 = abs(rand());
+  int m = alphabet.length();
+  int n = abs(rand()) % m; // future keyword length
+  int Ti = abs((A * T0 + C) % m); // if remove abs then strange errors occure
+  int Tplus1 = 0;
+  for(int i = 0; i < n; i++)
+  {
+    progress_bar(i + 1, n);
+    wchar_t ch = alphabet[Ti];
+    if(ch == L' ' || ch == L'\n' || ch == L'\t')
+    {
+      i--;
+      Tplus1 = (A * Ti + C) % m;
+      Ti = Tplus1;
+      continue;
+    }
+    keyword.push_back(ch);
+    Tplus1 = (A * Ti + C) % m;
+    Ti = Tplus1;
+  }
+  wcout << endl;
+}
+
+GamXorSeq::GamXorSeq(string operationfile_val, int A_val, int C_val)
+{
+  operationfile = operationfile_val;
+  A = A_val;
+  C = C_val;
+  keyword = L"";
+  alphabet = L"";
+  menu = true;
+}
+
+GamXorSeq::GamXorSeq(string operationfile_val)
+{
+  operationfile = operationfile_val;
+  A = 0;
+  C = 0;
+  keyword = L"";
+  alphabet = L"";
+  menu = false;
+}
+
+GamXorSeq::~GamXorSeq()
+{}
+
+void GamXorSeq::codefile()
+{
+  if(menu)
+  {
+    wifstream win(operationfile, ios::in);
+    win.imbue( locale("ru_RU.UTF-8") );
+    win.unsetf(ios::skipws);
+
+    wchar_t c;
+    wchar_t ch;
+    wctrans_t x = wctrans("tolower");
+    int cnt = 0;
+    set<wchar_t> reader;
+    while(win >> c)
+    {
+      cnt++;
+      ch = towctrans(c, x);
+      reader.insert(ch);
+    }
+    alphabet.assign(reader.begin(), reader.end());
+    setkeyword();
+
+    win.clear();
+    win.seekg(0, ios::beg);
+
+    string filename = operationfile.substr(0, operationfile.find('.')) + ".coded";
+    wofstream wout(filename, ios::out);
+    wout.imbue( locale("ru_RU.UTF-8") );
+
+    wout << keyword << L" " << alphabet.size() << L" ";
+    for(auto it = alphabet.begin(); it != alphabet.end();it++)
+    {
+      wout << wcconvert((*it)) << L" ";
+    }
+    
+    wcout << L" Кодирование" << endl;
+    int kwp = 0; // keyword position - i
+    int end = cnt;
+    cnt = 0;
+    while(win >> c)
+    {
+      cnt++;
+      progress_bar(cnt, end);
+      ch = towctrans(c, x);
+      wout << (wchar_t)((int)ch ^ (int)keyword[kwp]);
+      kwp = (kwp + 1) == (int)keyword.length() ? 0 : (kwp + 1);
+    }
+
+    win.close();
+    wout.close();
+  }
+  else
+  {
+    wcerr << L" Error: wrong menu choosen" << endl;
+    raise (SIGTERM);
+  }
+}
+
+void GamXorSeq::decodefile()
+{
+  if(!menu)
+  {
+    wifstream win(operationfile, ios::in);
+    win.imbue( locale("ru_RU.UTF-8") );
+
+    win >> keyword;
+    int n;
+    win >> n;
+    for(int i = 0; i < n; i++)
+    {
+      wstring s;
+      win >> s;
+      alphabet.push_back(wcdeconvert(s));
+    }
+
+    win.unsetf(ios::skipws);
+    string filename = operationfile.substr(0, operationfile.find('.')) + ".decoded";
+    wofstream wout(filename, ios::out);
+    wout.imbue( locale("ru_RU.UTF-8") );
+
+    wchar_t c;
+    win >> c;
+    
+    int kws = 0;
+    while(win >> c)
+    {
+      wout << (wchar_t)((int)c ^ (int)keyword[kws]);
+      kws = (kws + 1) == (int)keyword.size() ? 0 : (kws + 1);
+    }
+
+    win.close();
+    wout.close();
+  }
+  else
+  {
+    wcerr << L" Error: wrong menu choosen" << endl;
+    raise (SIGTERM);
+  }
+}
+
+wostream &operator<<(wostream &stream, GamXorSeq o)
+{
+  stream << endl;
+  wstring menu_name = (o.menu ? L"кодирование" : L"декодирование");
+  int len = max(menu_name.size(), o.keyword.size());
+  stream << L" Меню: " << setw(len) << menu_name << endl;
+  stream << L" Ключевое слово: " << setw(len) << o.keyword << endl;
+  stream << L" Алфавит: ";
+  for(auto it = o.alphabet.begin(); it != o.alphabet.end(); it++)
+  {
+    stream << setw(2) << wcconvert((*it)) << L" ";
+  }
+  return stream;
+}
